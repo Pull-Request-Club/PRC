c7643534217efe47ab60432f6c11b6f4b100f8ae
Kivanc Yazan <kyzn@cpan.org>
2018-10-13 17:23:22 +0300

    Add database setup

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Sign up to receive monthly GitHub issues as your homework!
 
 ## Development
 
+Building the database:
+
+    sqlite3 prc.db < prc.sql
+
+Adding a new modal:
+
+    script/prc_create.pl model DB DBIC::Schema PRC::Schema create=static dbi:SQLite:prc.db on_connect_do="PRAGMA foreign_keys = ON"
+
 Run the app locally:
 
     script/prc_server.pl
---
+++ prc.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS user(
+  id           INTEGER PRIMARY KEY,
+  create_time  DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time  DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  name         VARCHAR(255)
+);
+
+CREATE TRIGGER [UpdateLastTime]
+  AFTER UPDATE ON user FOR EACH ROW WHEN NEW.update_time <= OLD.update_time
+BEGIN
+  update user set update_time=CURRENT_TIMESTAMP where id=OLD.id;
+END;
