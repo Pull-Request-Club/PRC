0c13940c0377034bb4e6283ec2d3209d03babe04
Kivanc Yazan <kyzn@cpan.org>
2018-12-28 22:13:40 -0800

    Add new result class for Assignment
    Also update result classes for User and Repo
    to point to assignment table.
    
    Includes new methods too.

---
+++ lib/PRC/Schema/Result/Assignment.pm
@@ -0,0 +1,105 @@
+use utf8;
+package PRC::Schema::Result::Assignment;
+
+=head1 NAME
+
+PRC::Schema::Result::Assignment
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+use PRC::Constants;
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp");
+__PACKAGE__->table("assignment");
+__PACKAGE__->add_columns(
+  "assignment_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "repo_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "user_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "create_time",
+  {
+    data_type     => "datetime",
+    default_value => \"current_timestamp",
+    is_nullable   => 0,
+    set_on_create => 1,
+  },
+  "update_time",
+  {
+    data_type     => "datetime",
+    default_value => \"current_timestamp",
+    is_nullable   => 0,
+    set_on_update => 1,
+  },
+  "month",
+  {
+    data_type     => "datetime",
+    default_value => \"current_timestamp",
+    is_nullable   => 0,
+  },
+  "status",
+  { data_type => "integer", default_value => 0, is_nullable => 0 },
+);
+
+__PACKAGE__->set_primary_key("assignment_id");
+
+__PACKAGE__->belongs_to(
+  "repo",
+  "PRC::Schema::Result::Repo",
+  { repo_id => "repo_id" },
+  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
+);
+
+__PACKAGE__->belongs_to(
+  "user",
+  "PRC::Schema::Result::User",
+  { user_id => "user_id" },
+  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
+);
+
+=head1 METHODS
+
+=head2 status_string
+
+Returns text representation for status integer.
+
+=cut
+
+sub status_string {
+  my ($assignment) = @_;
+  my $status = $assignment->status;
+  if ($status == ASSIGNMENT_OPEN){
+    return "Open";
+  } elsif ($status == ASSIGNMENT_SKIPPED){
+    return "Skipped";
+  } elsif ($status == ASSIGNMENT_DONE){
+    return "Done";
+  } else {
+    return "Unknown";
+  }
+}
+
+=head2 month_pretty
+
+Returns pretty string for month row like "January 2019".
+
+=cut
+
+sub month_pretty {
+  my ($assignment) = @_;
+  my $month  = $assignment->month;
+  my $pretty = $month->month_name . ' ' . $month->year;
+  return $pretty;
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
