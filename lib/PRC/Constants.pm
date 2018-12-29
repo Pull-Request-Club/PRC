4c232c2f8e0c0ff8ade0d88a49e0bb4814e533e8
Kivanc Yazan <kyzn@cpan.org>
2018-12-28 22:14:12 -0800

    Add new constants for assignment statuses

--- lib/PRC/Constants.pm
+++ lib/PRC/Constants.pm
@@ -27,6 +27,10 @@ use constant USER_ASSIGNMENT_ACTIVE => 10;
 use constant USER_ASSIGNEE_QUIT   => 0;
 use constant USER_ASSIGNEE_ACTIVE => 10;
 
+use constant ASSIGNMENT_OPEN    => 0;
+use constant ASSIGNMENT_SKIPPED => 1;
+use constant ASSIGNMENT_DONE    => 10;
+
 our @EXPORT = qw/
   LATEST_LEGAL_DATE
 
@@ -36,6 +40,10 @@ our @EXPORT = qw/
 
   USER_ASSIGNEE_QUIT
   USER_ASSIGNEE_ACTIVE
+
+  ASSIGNMENT_OPEN
+  ASSIGNMENT_SKIPPED
+  ASSIGNMENT_DONE
 /;
 
 1;
