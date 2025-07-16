efbdf7de323ddf726e35768d57dd2f0118647325
Kivanc Yazan <kyzn@cpan.org>
2025-07-16 22:31:34 +0900

    Note about no more emails

--- lib/PRC/Email.pm
+++ lib/PRC/Email.pm
@@ -205,7 +205,7 @@ sub send_new_feature_email {
 
   my $body;
   my $to = $user->github_email;
-  my $subject = 'Reminder: Pull Request Club T-shirt Survey';
+  my $subject = '[Pull Request Club] Upcoming Changes to Email Notifications';
   my $unsub_link = PRC::Crypt->create_unsubscribe_link($user_id, $email_id);
 
   my $tt = Template->new({
